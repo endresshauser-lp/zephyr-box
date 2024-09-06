b0aa0cce252b22888ba0051ef0a08fd835cc8d62
Cla Mattia Galliard <cla-mattia.galliard@zuehlke.com>
2024-09-06 18:23:25 +0200

    Add ST support, simplify setup
    - Adjust and cleanup run script. It can now be used for both setting up
      a bash shell and for running a specific command. If no command is
      provided the image will run `bash` as default.
    - Adds pycocd for flashing and debugging. Tested for stm32.
    - Adjust documentation
    - Dockerfile
      - Dockerfile: Single step for apt-packages. They change rarely.
        Remove some unnecessary packages to make image smaller. Note, that
        [clean is done automatically on images base on
        Ubuntu](https://docs.docker.com/build/building/best-practices/#apt-get).
      - Configure minicom
      - Remove entrypoint in favor of directly calling on_docker_startup.
      - Install zephyr and mcuboot python requirements with the typical
        requirements.txt approach.
    
    Todo
    - test with vs-code devcontainer
    - test with nordic chips (pyocd)
    
    Full rebuild from scratch on my machine:
    - 260 seconds
    -   5 GB image
    
    Significant Layers:
    - 90s Apt
    - 23s Zephyr SDK
    - 31s Selenium Layer
    - 40s Puncover (including toolchain)
    - 25s zephyr pip packages
    - 34s pyOCD (including packs for stm and nrf)

---
+++ run_in_docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+USER_NAME=user
+USER_UID=$(id -u "$(whoami)")
+USER_GID=$(id -g "$(whoami)")
+
+# SCRIPT_DIR needs to be the directory containing Dockerfile and a subdirectory of
+# the PROJECT_ROOT_HOST directory
+SCRIPT_DIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+PROJECT_ROOT_HOST=$(realpath "$(dirname "$SCRIPT_DIR")")
+WEST_WORKSPACE_HOST=$PROJECT_ROOT_HOST/.west_workspace
+HOME_CONTAINTER=/home/$USER_NAME
+WEST_WORKSPACE_CONTAINER=$HOME_CONTAINTER/west_workspace
+PROJECT_ROOT_CONTAINER=$WEST_WORKSPACE_CONTAINER/project
+
+mkdir -p "$WEST_WORKSPACE_HOST"
+
+docker build \
+    --network host \
+    --build-arg="USER_NAME=$USER_NAME" \
+    --build-arg="UID=$USER_UID" \
+    --build-arg="GID=$USER_GID" \
+    --build-arg="PROJECT_DIR=$PROJECT_ROOT_CONTAINER" \
+    --tag zephyr-box \
+    "$SCRIPT_DIR"
+
+docker run \
+    --network host --tty --interactive --rm --privileged \
+    --volume ~/.ssh:/home/user/.ssh \
+    --volume /dev:/dev \
+    --volume "$WEST_WORKSPACE_HOST:$WEST_WORKSPACE_CONTAINER" \
+    --volume "$PROJECT_ROOT_HOST:$PROJECT_ROOT_CONTAINER" \
+    zephyr-box "$@"
